d5115a1320ac2688166ddfbe7bd8243909d1f253
aaron blum <blumaa@gmail.com>
2019-10-14 13:31:22 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(bowls) {
+
+  // return bowls.find( (bowl) => {
+  //   if (bowl.result === "W") {
+  //     return bowl.year
+  //   }
+  // })
+  // if (found) return found.year
+  const found = bowls.find( (bowl) => { return bowl.result === "W" })
+  if (found) return found.year
+}
